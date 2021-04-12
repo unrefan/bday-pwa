380817d47c323335837c0763f10b2dad35075381
Pomazan Vladislav <pomazanvi@gmail.com>
2021-04-12 21:28:05 +0300

    pwa

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,34 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: 'autoUpdate',  
+      manifest: {
+        "short_name": "Bdays",
+        "name": "Remember bdays",
+        "icons": [
+          // {
+          //   "src": "/images/icons-192.png",
+          //   "type": "image/png",
+          //   "sizes": "192x192"
+          // },
+          // {
+          //   "src": "/images/icons-512.png",
+          //   "type": "image/png",
+          //   "sizes": "512x512"
+          // }
+        ],
+        "start_url": "/?source=pwa",
+        "background_color": "#3367D6",
+        "display": "standalone",
+        "scope": "/",
+        "theme_color": "#3367D6",
+        "description": "Birthdays information",
+      },
+    }),
+  ]
 })
